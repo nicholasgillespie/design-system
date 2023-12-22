18684f3df1c535fd709a7878c1be5fb937cb5752
Nicholas Gillespie <contact@nicholasgillespie.com>
2023-12-22 09:05:27 +0100

    Update server.js to use dynamic port

--- server.js
+++ server.js
@@ -8,9 +8,10 @@ import app from "./backend/app.js";
 /* CREATE SERVER //////////////////// */
 const server = http.createServer(app);
 const { SITE_PROTO, SITE_HOST, SITE_PORT } = process.env;
+const port = SITE_PORT || 3000;
 
 /* START SERVER //////////////////// */
-server.listen(SITE_PORT, () => {
+server.listen(port, () => {
   const date = new Date().toLocaleString("fr-FR", { timeZone: "Europe/Paris" });
   console.log(
     `${date} - Application running at ${SITE_PROTO}://${SITE_HOST}:${SITE_PORT}`
