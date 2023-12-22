8b77a869cd74684497abe8029f2ba7fa616f7610
Nicholas Gillespie <contact@nicholasgillespie.com>
2023-12-22 11:51:28 +0100

    Add whitelist for dark and light themes

--- purgecss.config.cjs
+++ purgecss.config.cjs
@@ -1,4 +1,5 @@
 module.exports = {
   content: ["./frontend/**/*.ejs", "./frontend/**/*.js"],
   css: ["./frontend/public/css/*.css"],
+  whitelist: ['body[data-theme="dark"]', 'body[data-theme="light"]'],
 };
