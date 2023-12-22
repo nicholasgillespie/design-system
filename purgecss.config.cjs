fd32cb6d7b97804d34e4b86a2532eaa6d98ccef3
Nicholas Gillespie <contact@nicholasgillespie.com>
2023-12-22 12:02:38 +0100

    Remove unused PostCSS and PurgeCSS configurations

--- purgecss.config.cjs
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  content: ["./frontend/**/*.ejs", "./frontend/**/*.js"],
-  css: ["./frontend/public/css/*.css"],
-  whitelist: ['body[data-theme="dark"]', 'body[data-theme="light"]'],
-};
