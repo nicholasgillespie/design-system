b93e18cfbc1abd6bea4f026cf4910399f7d41db9
Nicholas Gillespie <contact@nicholasgillespie.com>
2023-12-22 09:14:45 +0100

    Update port

--- server.js
+++ server.js
@@ -7,13 +7,12 @@ import app from "./backend/app.js";
 
 /* CREATE SERVER //////////////////// */
 const server = http.createServer(app);
-const { SITE_PROTO, SITE_HOST, SITE_PORT } = process.env;
-const port = SITE_PORT || 3000;
+// const { PROTO, HOST, PORT } = process.env;
+const port = process.env.PORT || 3000;
 
 /* START SERVER //////////////////// */
 server.listen(port, () => {
   const date = new Date().toLocaleString("fr-FR", { timeZone: "Europe/Paris" });
-  console.log(
-    `${date} - Application running at ${SITE_PROTO}://${SITE_HOST}:${SITE_PORT}`
-  );
+  // console.log(`${date} - Application running at ${PROTO}://${HOST}:${PORT}`);
+  console.log(`${date}`);
 });
